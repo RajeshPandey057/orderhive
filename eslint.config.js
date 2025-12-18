47774121b983753daa8fc5608c49daf98679185f
Raj <rajesh-pandey@outlook.in>
2025-12-18 13:01:03 +0530

    feat: ✨ Add new UI components for tables, tabs, tooltips, and toggle groups
    - Introduced `table-row.svelte` and `table.svelte` for table structures.
    - Created tab components: `tabs.svelte`, `tabs-content.svelte`, `tabs-list.svelte`, and `tabs-trigger.svelte`.
    - Added tooltip components: `tooltip.svelte`, `tooltip-trigger.svelte`, `tooltip-content.svelte`, `tooltip-provider.svelte`, and `tooltip-portal.svelte`.
    - Implemented `toggle-group` and `toggle` components for toggle functionality.
    - Enhanced utility functions in `utils.ts` for class name management.
    - Established a mobile detection hook in `is-mobile.svelte.ts`.
    - Updated global styles in `layout.css` to support new components and themes.

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-import prettier from 'eslint-config-prettier';
-import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
 import { defineConfig } from 'eslint/config';
 import globals from 'globals';
+import { fileURLToPath } from 'node:url';
 import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 
@@ -18,17 +18,26 @@ export default defineConfig(
 	prettier,
 	...svelte.configs.prettier,
 	{
-		languageOptions: { globals: { ...globals.browser, ...globals.node } },
-
+		languageOptions: {
+			globals: { ...globals.browser, ...globals.node }
+		},
 		rules: {
 			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
 			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
+			'no-undef': 'off',
+			'svelte/no-navigation-without-resolve': [
+				'off',
+				{
+					ignoreGoto: false,
+					ignoreLinks: false,
+					ignorePushState: false,
+					ignoreReplaceState: false
+				}
+			]
 		}
 	},
 	{
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
-
 		languageOptions: {
 			parserOptions: {
 				projectService: true,
--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,11 @@
+import 'unplugin-icons/types/svelte';
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
+	type APIResponse<T = object> = { success: false; error: string } | { success: true; data: T };
+	namespace Superforms {
+		type Message = { type: 'error' | 'success' | 'warning'; text: string };
+	}
 	namespace App {
 		// interface Error {}
 		// interface Locals {}
