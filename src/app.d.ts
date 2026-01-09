a40b79537d820d099f89a44049c5d2ec88eea052
Raj <rajesh-pandey@outlook.in>
2026-01-09 11:40:06 +0530

    Revert "chore: ✨ update project configuration for Cloudflare deployment"
    This reverts commit 4759ca47eb4d5e55d092117ca52696b64e2a8a91.

--- src/app.d.ts
+++ src/app.d.ts
@@ -249,20 +249,7 @@ declare global {
 		type Message = { type: 'error' | 'success' | 'warning'; text: string };
 	}
 	namespace App {
-        interface Platform {
-            env: Env
-            cf: CfProperties
-            ctx: ExecutionContext
-        }
-
-        interface Platform {
-			env: Env;
-			ctx: ExecutionContext;
-			caches: CacheStorage;
-			cf?: IncomingRequestCfProperties;
-		}
-
-        // interface Error {}
+		// interface Error {}
 		interface Locals {
 			user: {
 				uid: string;
@@ -270,10 +257,10 @@ declare global {
 				role: 'admin' | 'agent' | 'compliance' | 'finance' | 'super-admin';
 			} | null;
 		}
-        // interface PageData {}
-        // interface PageState {}
-        // interface Platform {}
-    }
+		// interface PageData {}
+		// interface PageState {}
+		// interface Platform {}
+	}
 }
 
 export {};
